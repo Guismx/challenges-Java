e13cd1c426fbdbf4a5cdc2b603567cebfa677bcb
Guismx <Guilhermesmx@gmail.com>
2024-08-15 15:00:47 -0300

    New challenge 1.1.13

---
+++ src/easychallenges/challenge1_1_13.java
@@ -0,0 +1,31 @@
+//1.1.13 Pinheiro 1 - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+//Enriqueça o desenho com outros caracteres, simulando enfeites.
+//              X
+//             XXX
+//            XXXXX
+//           XXXXXXX
+//          XXXXXXXXX
+//         XXXXXXXXXXX
+//        XXXXXXXXXXXXX
+//       XXXXXXXXXXXXXXX
+//             XX
+//             XX
+//            XXXX
+
+package easychallenges;
+
+public class challenge1_1_13 {
+    public static void main(String[] args) {
+        System.out.println("              J");
+        System.out.println("             XJX");
+        System.out.println("            XXJXX");
+        System.out.println("           XXJXXXX");
+        System.out.println("          XXJXXXXXX");
+        System.out.println("         XXXXXXJXXXX");
+        System.out.println("        XXXJXXXXXJXXX");
+        System.out.println("       XXXXJXXXXXXXXXX");
+        System.out.println("             XX");
+        System.out.println("             XX");
+        System.out.println("            XXXX");
+    }
+}
