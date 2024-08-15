487064dc38edcc1c7ba2a97877d1ac5250621275
Guismx <Guilhermesmx@gmail.com>
2024-08-15 14:43:03 -0300

    New challenge 1.1.11

---
+++ src/easychallenges/challenge1_1_11.java
@@ -0,0 +1,20 @@
+//1.1.11 Desenho datilográfico - Há um artista que faz desenhos somente com caracteres disponíveis
+//em uma máquina de escrever. Bole uma figura nesse estilo (bem melhor que a abaixo, se possível)
+//e faça um programa que a mostre na tela.
+//BÁSICOS 3
+//    ____
+//   /    \
+//  /  o o \
+//  \   ^  /
+//   \__~_/
+
+
+package easychallenges;
+
+public class challenge1_1_11 {
+    public static void main(String[] args) {
+        System.out.println("  /\\_/\\  ");
+        System.out.println(" ( o.o ) ");
+        System.out.println("  > ^ <  ");
+    }
+}
