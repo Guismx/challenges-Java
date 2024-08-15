86ac0ba9d18bc1e77b7f265e41840c7efe490571
Guismx <Guilhermesmx@gmail.com>
2024-08-15 12:48:02 -0300

    New challenge 1.1.10

---
+++ src/easychallenges/challenge1_1_10.java
@@ -0,0 +1,103 @@
+//1.1.10 Palavra grande - Escreva um programa que produza a tela esquematizada abaixo:
+// ********     ********    *
+// *            *      *    *
+// *            *      *    *
+// ********     *      *    *
+//        *     *      *    *
+//        *     *      *    *
+// ********     ********     ********
+//
+//
+package easychallenges;
+
+public class challenge1_1_10 {
+    public static void main(String[] args) {
+
+        int[] firstLine = new int[3];
+        firstLine[0] = 8;
+
+        for (int i = 0; i < firstLine[0]; i++) {
+            System.out.print("*");
+        }
+        System.out.print(" ");
+
+        for (int j = 0; j < firstLine[0]; j++) {
+            System.out.print("*");
+        }
+        System.out.println(" *");
+
+        System.out.print("*");
+        for (int i = 0; i < 8; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+
+        for (int i = 0; i < 6; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        System.out.println(" *");
+
+        System.out.print("*");
+        for (int i = 0; i < 8; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+
+        for (int i = 0; i < 6; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        System.out.println(" *");
+
+        for (int i = 0; i < firstLine[0]; i++) {
+            System.out.print("*");
+        }
+        System.out.print(" ");
+        System.out.print("*");
+
+        for (int i = 0; i < 6; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        System.out.println(" *");
+
+        for (int i = 0; i < 7 ; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        System.out.print(" *");
+
+        for (int i = 0; i < 6 ; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        System.out.println(" *");
+
+        for (int i = 0; i < 7 ; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        System.out.print(" *");
+
+        for (int i = 0; i < 6 ; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        System.out.println(" *");
+
+        for (int i = 0; i < firstLine[0]; i++) {
+            System.out.print("*");
+        }
+        System.out.print(" ");
+
+        for (int j = 0; j < firstLine[0]; j++) {
+            System.out.print("*");
+        }
+        System.out.print(" ");
+
+        for (int j = 0; j < firstLine[0]; j++) {
+            System.out.print("*");
+        }
+    }
+}
