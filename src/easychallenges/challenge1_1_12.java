a88c8e2a5ca5a5f0ef0666e424b9cbd4e43c9d53
Guismx <Guilhermesmx@gmail.com>
2024-08-15 14:47:49 -0300

    New challenge 1.1.12

---
+++ src/easychallenges/challenge1_1_12.java
@@ -0,0 +1,34 @@
+//1.1.12 Emoticons - Emoticons são seqüências de caracteres que mostram
+// rostos e expressões, vistos de lado e usados freqüentemente em correios
+// eletrônicos e bate-papos na Internet. Existem dezenas;
+// Veja alguns:
+//:-) sorriso
+//:-( tristeza
+//:-p mostrando a língua
+//:-o espanto
+//{:-) cabelo partido ao meio
+//:-{ usa bigode
+//:-* beijo
+//
+// Elabore um programa que mostre na tela os emoticons, um em cada linha, com a
+// descrição de cada um.
+
+package easychallenges;
+
+public class challenge1_1_12 {
+    public static void main(String[] args) {
+        System.out.println(":-) sorriso");
+        System.out.println("------------------------");
+        System.out.println(":-( tristeza");
+        System.out.println("------------------------");
+        System.out.println(":-p mostrando a língua");
+        System.out.println("------------------------");
+        System.out.println(":-o espanto");
+        System.out.println("------------------------");
+        System.out.println("{:-) cabelo partido ao meio");
+        System.out.println("------------------------");
+        System.out.println(":-{ usa bigode");
+        System.out.println("------------------------");
+        System.out.println(":-* beijo");
+    }
+}
