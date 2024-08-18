588fd915c02392bb7626a758fded3726088f2a53
Guismx <Guilhermesmx@gmail.com>
2024-08-17 22:35:24 -0300

    test

--- src/basics/mathematics/challenge1_2_2.java
+++ src/basics/mathematics/challenge1_2_2.java
@@ -29,7 +29,7 @@ public class challenge1_2_2 {
             System.out.println("11 div 3 mod 2 = " + calc3);
             System.out.println("11 mod 3 div 2 = " + calc4);
         } else {
-            System.out.println("has incorrect calculations");
-        }
+
+        } System.out.println("has incorrect calculations");
     }
 }
